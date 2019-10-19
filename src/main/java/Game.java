064ff56e76fb028711f01cfd86ce7105d21dc0d0
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-19 13:57:03 +0100

    can add and remove players to/from game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,29 @@
+import people.Person;
+
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Person> players;
+
+    public Game() {
+        this.players = new ArrayList<Person>();
+    }
+
+    public int countPlayers() {
+        return this.players.size();
+    }
+
+
+    public void addPlayer(Person player) {
+        this.players.add(player);
+    }
+
+    public void removePlayer(Person player) {
+        this.players.remove(player);
+    }
+
 }
-
-
 // there are different ways to do a fight.
 // one way is just to compare the power of each fighter.
 // another way is to use rock paper scissors method
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,54 @@
+import com.sun.tools.javac.comp.Enter;
+import org.junit.Before;
+import org.junit.Test;
+import people.Entertainer;
+import people.Person;
+import people.Warrior;
+import people.Wizard;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class GameTest {
 
+    Game game;
+    Wizard player1;
+    Warrior player2;
+    Entertainer player3;
+
+    @Before
+    public void before() {
+        game = new Game();
+        player1 = new Wizard("Thelma");
+        player2 = new Warrior("Louise");
+        player3 = new Entertainer("Lady Gaga");
+
+    }
+
+    //game has a list of players, and this list starts empty
+    @Test
+    public void gameStartsWithNoPlayers() {
+        assertEquals(0, game.countPlayers());
+    }
+
+    //can add players to the list of players
+    @Test
+    public void canAddPlayer() {
+        game.addPlayer(player1);
+        assertEquals(1, game.countPlayers());
+    }
+
+    //can remove players from the list of players (that is, players die)
+    @Test
+    public void canRemovePlayer() {
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+        game.removePlayer(player1);
+        assertEquals(1, game.countPlayers());
+
+    }
+
+    //players can fight an enemy
+
+    //there is a winner (the last player standing) - unless they all get killed!
 }
